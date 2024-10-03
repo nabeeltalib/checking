cf7c457fb547a711fc2c4f6ff2bb32aba53d1614
MikeA <meal@ix.netcom.com>
2024-10-03 17:59:23 -0400

    mike edit 10-3 v9

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-// src/App.tsx
-
 import { useEffect, useState } from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import {
   Home,
   Explore,
@@ -45,7 +43,7 @@ import SigninForm from "@/_auth/forms/SigninForm";
 import { OnboardingPage, EmbedPreview, EmbedSelector } from "@/components/shared";
 import Notifications from "./components/shared/Notifications";
 import AuthCallback from './_root/pages/AuthCallback';
-import NotFound from "./_root/pages/NotFound"; // Ensure you have a NotFound component
+import NotFound from "./_root/pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
@@ -77,88 +75,88 @@ const App: React.FC<AppProps> = ({ isInitialLoading }) => {
   }
 
   return (
-    <main className="flex">
-    <ShareDialogProvider>
-
-      <Routes>
-        {/* Public Routes */}
-        <Route element={<AuthLayout />}>
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<OnboardingScreens />} />        
-        </Route>
-        <Route path="/staticframe/:id" element={<StaticFrame />} />
-        <Route path="/liveframe/:id" element={<LiveFrame />} />
-        
-        {/* Authenticated Routes */}
-        {user.id ? (
-          <Route element={<RootLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/explore" element={<Explore />} />
-            <Route path="/embed" element={<EmbedSelector />} />
-            <Route path="/embedpreview/:id" element={<EmbedPreview />} />
-            <Route path="/notifications" element={<Notifications />} />
-            <Route path="/categories/:value" element={<Categories />} />
-            <Route path="/collaborations" element={<Collaborations />} />
-            <Route path="/listfromfriends" element={<ListFromFriends />} />
-            <Route path="/userlists" element={<Userlist />} />
-            <Route path="/trending" element={<Trending />} />
-            <Route path="/recommended" element={<Recomended />} />
-            <Route path="/userActivity" element={<UserActivity />} />
-            <Route path="/saved" element={<Saved />} />
-            <Route path="/all-lists" element={<AllLists />} />
-            <Route path="/create-list" element={<CreateList />} />
-            <Route path="/update-list/:id" element={<EditList />} />
-            <Route path="/lists/:id" element={<ListDetails />} />
-            <Route path="/profile/:profile" element={<Profile />} />
-            <Route path="/update-profile/:id" element={<UpdateProfile />} />
-            <Route path="/lists/:id/comments" element={<Comments />} />
-            <Route path="/lists/:id/suggestions" element={<Suggestions />} />
-            <Route path="/remix/:id" element={<RemixList />} /> {/* New route */}
-            <Route path="/manage-list/:id" element={<ManageList />} /> {/* New route */}
-            <Route path="/segment-leaderboard" element={<SegmentedLeaderboardPage />} /> {/* New route */}
-            <Route path="/profile-screen" element={<ProfileScreen />} /> {/* New route */}
-            <Route path="/helpfaqpage" element={<HelpFAQPage />} />
-            <Route path="/contactpage" element={<ContactPage />} />          
-            <Route path="/comprehensive-leaderboard" element={<ComprehensiveLeaderboard />} /> {/* New route */}
-            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-            <Route path="/termsandconditions" element={<TermsAndConditions />} />
-            <Route path="/cookiepolicy" element={<CookiePolicy />} />
-            <Route path="/explore2" element={<Explore2 />} />
-            <Route path="/admin-panel" element={<AdminPanel />} />
-            <Route path="/auth/callback" element={<AuthCallback />} />
+    <>
+      <main className="flex">
+        <ShareDialogProvider>
+          <Routes>
+            {/* Public Routes */}
+            <Route element={<AuthLayout />}>
+              <Route path="/sign-in" element={<SigninForm />} />
+              <Route path="/sign-up" element={<OnboardingScreens />} />        
+            </Route>
+            <Route path="/staticframe/:id" element={<StaticFrame />} />
+            <Route path="/liveframe/:id" element={<LiveFrame />} />
             
-            {/* Shared Route within Authenticated Layout */}
-            <Route path="/shared/:sharedId" element={<SharedListView />} />
-          </Route>
-        ) : (
-          /* Unauthenticated Routes */
-          <Route element={<RootLayout2 />}>
-            <Route path="/" element={<PreviewMode />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/explore2" element={<Explore2 />} />
-            <Route path="/discover" element={<AllLists />} />
-            <Route path="/trending" element={<Trending />} />
-            <Route path="/recommended" element={<Recomended />} />
-            <Route path="/onboarding" element={<OnboardingScreens />} /> 
-            <Route path="/onboarding-page" element={<OnboardingPage />} /> {/* New route */}
-            <Route path="/helpfaqpage" element={<HelpFAQPage />} />
-            <Route path="/contactpage" element={<ContactPage />} />          
-            <Route path="/comprehensive-leaderboard2" element={<ComprehensiveLeaderboard2 />} /> {/* New route */}
-            <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-            <Route path="/termsandconditions" element={<TermsAndConditions />} />
-            <Route path="/cookiepolicy" element={<CookiePolicy />} />
-            {/* Shared routes for unauthenticated users */}
-            <Route path="/shared/:sharedId" element={<SharedListView />} />
-          </Route>
-        )}
+            {/* Authenticated Routes */}
+            {user.id ? (
+              <Route element={<RootLayout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/explore" element={<Explore />} />
+                <Route path="/embed" element={<EmbedSelector />} />
+                <Route path="/embedpreview/:id" element={<EmbedPreview />} />
+                <Route path="/notifications" element={<Notifications />} />
+                <Route path="/categories/:value" element={<Categories />} />
+                <Route path="/collaborations" element={<Collaborations />} />
+                <Route path="/listfromfriends" element={<ListFromFriends />} />
+                <Route path="/userlists" element={<Userlist />} />
+                <Route path="/trending" element={<Trending />} />
+                <Route path="/recommended" element={<Recomended />} />
+                <Route path="/userActivity" element={<UserActivity />} />
+                <Route path="/saved" element={<Saved />} />
+                <Route path="/all-lists" element={<AllLists />} />
+                <Route path="/create-list" element={<CreateList />} />
+                <Route path="/update-list/:id" element={<EditList />} />
+                <Route path="/lists/:id" element={<ListDetails />} />
+                <Route path="/profile/:profile" element={<Profile />} />
+                <Route path="/update-profile/:id" element={<UpdateProfile />} />
+                <Route path="/lists/:id/comments" element={<Comments />} />
+                <Route path="/lists/:id/suggestions" element={<Suggestions />} />
+                <Route path="/remix/:id" element={<RemixList />} /> {/* New route */}
+                <Route path="/manage-list/:id" element={<ManageList />} /> {/* New route */}
+                <Route path="/segment-leaderboard" element={<SegmentedLeaderboardPage />} /> {/* New route */}
+                <Route path="/profile-screen" element={<ProfileScreen />} /> {/* New route */}
+                <Route path="/helpfaqpage" element={<HelpFAQPage />} />
+                <Route path="/contactpage" element={<ContactPage />} />          
+                <Route path="/comprehensive-leaderboard" element={<ComprehensiveLeaderboard />} /> {/* New route */}
+                <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+                <Route path="/termsandconditions" element={<TermsAndConditions />} />
+                <Route path="/cookiepolicy" element={<CookiePolicy />} />
+                <Route path="/explore2" element={<Explore2 />} />
+                <Route path="/admin-panel" element={<AdminPanel />} />
+                <Route path="/auth/callback" element={<AuthCallback />} />
+                
+                {/* Shared Route within Authenticated Layout */}
+                <Route path="/shared/:sharedId" element={<SharedListView />} />
+              </Route>
+            ) : (
+              /* Unauthenticated Routes */
+              <Route element={<RootLayout2 />}>
+                <Route path="/" element={<PreviewMode />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/explore2" element={<Explore2 />} />
+                <Route path="/discover" element={<AllLists />} />
+                <Route path="/trending" element={<Trending />} />
+                <Route path="/recommended" element={<Recomended />} />
+                <Route path="/onboarding" element={<OnboardingScreens />} />
+                <Route path="/onboarding-page" element={<OnboardingPage />} /> {/* New route */}
+                <Route path="/helpfaqpage" element={<HelpFAQPage />} />
+                <Route path="/contactpage" element={<ContactPage />} />          
+                <Route path="/comprehensive-leaderboard2" element={<ComprehensiveLeaderboard2 />} /> {/* New route */}
+                <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+                <Route path="/termsandconditions" element={<TermsAndConditions />} />
+                <Route path="/cookiepolicy" element={<CookiePolicy />} />
+                <Route path="/shared/:sharedId" element={<SharedListView />} />
+              </Route>
+            )}
 
-        {/* Catch-all route */}
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <ShareDialog />
-      <Toaster />      
-    </ShareDialogProvider>
-    </main>
+            {/* Catch-all route */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+          <ShareDialog />
+          <Toaster />      
+        </ShareDialogProvider>
+      </main>
+    </>
   );
 };
 
